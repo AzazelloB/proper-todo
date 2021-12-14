8a8cd21b8db3d7b01f1448d996fdd37077c33d58
Eduard Diakunenko <eduard.diakunenko@kodex.lv>
2021-12-14 22:16:13 +0200

    fix checkbox handler

--- packages/core/src/components/TodoList/Todo.js
+++ packages/core/src/components/TodoList/Todo.js
@@ -1,13 +1,13 @@
 import './style.css'
 
 function Todo({ todo: { id, text, completed }, toggle }) {
-    const handleClick = () => {
+    const handleChange = () => {
         toggle(id)
     }
 
     return (
         <div className="todo">
-            <input id={`todo_${id}`} type="checkbox" checked={completed} onClick={handleClick} />
+            <input id={`todo_${id}`} type="checkbox" checked={completed} onChange={handleChange} />
             <label htmlFor={`todo_${id}`} className={completed ? 'completed' : ''}>{ text }</label>
         </div>
     )
